48258b0702b8191cc1e92d24723565b377582b3e
Mimo <mimoriam420@gmail.com>
2024-04-13 01:40:22 +0500

    Commit #3 - Enable Windows support

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app_flutter/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
@@ -13,8 +14,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       restorationScopeId: 'app',
-      home: Container(),
-      onGenerateTitle: (BuildContext context) => 'Shop',
+      home: const SafeArea(
+        child: Text("hello"),
+      ),
+      onGenerateTitle: (BuildContext context) => 'Shop'.hardcoded,
       theme: ThemeData(
         // * Use this to toggle Material 3 (defaults to true since Flutter 3.16)
         useMaterial3: true,
