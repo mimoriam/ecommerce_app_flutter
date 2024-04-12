b8ec15fb782b8d6b0b0bb8714e359be5244aa4e6
Mimo <mimoriam420@gmail.com>
2024-04-13 00:44:12 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      restorationScopeId: 'app',
+      home: Container(),
+      onGenerateTitle: (BuildContext context) => 'Shop',
+      theme: ThemeData(
+        // * Use this to toggle Material 3 (defaults to true since Flutter 3.16)
+        useMaterial3: true,
+        primarySwatch: Colors.grey,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.black87,
+          foregroundColor: Colors.white,
+          elevation: 0,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.black,
+            foregroundColor: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
